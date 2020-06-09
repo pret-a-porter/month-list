3c1e1e35fe1c531d878d476ace97ac6912ae3be7
Maksim Sharipov <m.v.sharipov@gmail.com>
2020-06-09 18:57:12 +0300

    Tests were added

---
+++ src/index.test.ts
@@ -0,0 +1,37 @@
+import { getMonthList } from "./index";
+
+describe("getMonthList", () => {
+  test("Should return list of month names", () => {
+    expect(getMonthList("en")).toEqual([
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]);
+  });
+
+  test("Should return list of short month names", () => {
+    expect(getMonthList("en", "short")).toEqual([
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ]);
+  });
+});
