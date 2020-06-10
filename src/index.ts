a3df6ae3d6df95d7fac7f58c9e0f48fb89a8c2ab
Maksim Sharipov <m.v.sharipov@gmail.com>
2020-06-10 13:26:25 +0300

    Code style

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-export const getMonthList = (l?: string | string[], f?: string): string[] =>
+export const getMonthList = (
+  l?: string | string[],
+  f?: 'long' | 'short' | 'numeric',
+): string[] =>
   Array.from(Array(12).keys(), (i) =>
     new Intl.DateTimeFormat(l, {
-      month: f || "long",
-    }).format(new Date(0, i))
+      month: f || 'long',
+    }).format(new Date(0, i)),
   );
